e8801a85ced089494a3f7502047d10543056ab79
Liam <liambale@gmail.com>
2021-02-23 18:37:34 +0000

    Add the functionality to level editor menu

--- Source/FrontEnd/LevelEditorMenuController.java
+++ Source/FrontEnd/LevelEditorMenuController.java
@@ -1,17 +1,16 @@
 package FrontEnd;
 
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.media.AudioClip;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.EventListener;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
  * Used to control the options for level editor
@@ -29,22 +28,33 @@ public class LevelEditorMenuController extends StateLoad{
     @FXML
     private Button backButton;
     @FXML
-    private RadioButton newButton;
+    private RadioButton newLevel;
     @FXML
-    private RadioButton existingButton;
+    private RadioButton existingLevel;
     @FXML
     private FlowPane existingOption;
     @FXML
     private ChoiceBox<String> selectGameBoard;
+    @FXML
+    private TextField nameIn;
+    @FXML
+    private TextField widthIn;
+    @FXML
+    private TextField heightIn;
 
 
-
+    /**
+     * Sets up objects in the class
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         ToggleGroup levelEditorOption = new ToggleGroup();
-        newButton.setToggleGroup(levelEditorOption);
-        existingButton.setToggleGroup(levelEditorOption);
+        newLevel.setToggleGroup(levelEditorOption);
+        existingLevel.setToggleGroup(levelEditorOption);
 
         existingOption.setDisable(true);
 
@@ -60,14 +70,8 @@ public class LevelEditorMenuController extends StateLoad{
             }
             selectGameBoard.getSelectionModel().selectFirst();
         }
-    }
 
-    /**
-     * Toggles
-     */
-    public void onLevelEditorToggle(){
-        levelEditorOption = !levelEditorOption;
-        existingOption.setDisable(levelEditorOption);
+        emptyTextBoxes();
     }
 
     /**
@@ -78,4 +82,129 @@ public class LevelEditorMenuController extends StateLoad{
         wl.load("MenuScreen", getInitData());
         RETURN_AUDIO.play(Double.parseDouble(getInitData().get("SFXVol")));
     }
+
+    /**
+     * Takes you to the level editor if requirements are met.
+     */
+    public void onStartButton() {
+
+        boolean inputError = false; //True if error
+
+        if (!correctBoardName(nameIn.getText())) {
+            nameIn.setText("");
+            nameIn.setStyle("-fx-prompt-text-fill:red;");
+            inputError = true;
+        }
+        if (!correctBoardSize(widthIn.getText())) {
+            widthIn.setText("");
+            widthIn.setStyle("-fx-prompt-text-fill:red;");
+            inputError = true;
+        }
+        if (!correctBoardSize(heightIn.getText())) {
+            heightIn.setText("");
+            heightIn.setStyle("-fx-prompt-text-fill:red;");
+            inputError = true;
+        }
+        if (inputError) {
+            return;
+        }
+
+        //Take you to level editor
+
+    }
+
+    /**
+     * Fills text boxes with pre-existing board data
+     */
+    public void boardSelect() {
+        nameIn.setText(selectGameBoard.getValue());
+
+        File loadFile = new File("Gameboards/" + selectGameBoard.getValue() + ".txt");
+        Scanner in = null;
+        try {
+            in = new Scanner(loadFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int width = in.nextInt();
+        int height = in.nextInt();
+
+        in.close();
+
+        widthIn.setText(String.valueOf(width));
+        heightIn.setText(String.valueOf(height));
+
+    }
+
+    /**
+     * Checks the board size in is a set size
+     * @param sizeIn The board input size
+     * @return True if size is valid, else False
+     */
+    private boolean correctBoardSize(String sizeIn) {
+
+        final int MAX_IN = 100;
+        final int MIN_IN = 3;
+        int numIn = 0;
+
+        try {
+            numIn = Integer.parseInt(sizeIn);
+        } catch (NumberFormatException e) {
+            System.out.println("NOT a int input");
+        }
+
+        if (numIn >= MIN_IN && numIn <= MAX_IN){
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks the board name is unique
+     * @param nameIn The name of the new board level
+     * @return True if the name is unique, else False
+     */
+    private boolean correctBoardName(String nameIn) {
+        String[] gameBoards;
+        File gameBoardLocation = new File("Gameboards");
+        gameBoards = gameBoardLocation.list();
+
+        for (String board : gameBoards) {
+            board = board.substring(0, board.length() - 4);
+
+            if (nameIn.equalsIgnoreCase(board)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Toggles
+     */
+    public void onLevelEditorToggle(){
+        levelEditorOption = !levelEditorOption;
+        existingOption.setDisable(levelEditorOption);
+
+        if (levelEditorOption) {
+            emptyTextBoxes();
+
+        }else {
+            boardSelect();
+        }
+    }
+
+    /**
+     * Empty all text box on level editor
+     */
+    private void emptyTextBoxes() {
+        nameIn.setText("");
+        widthIn.setText("");
+        heightIn.setText("");
+    }
+
+
 }
