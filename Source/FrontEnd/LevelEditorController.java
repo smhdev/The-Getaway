ecdae40d6cb2430cbecf3d2cbe72e5570c9a49ce
Liam <liambale@gmail.com>
2021-02-26 14:03:34 +0000

    Added slider movement

--- Source/FrontEnd/LevelEditorController.java
+++ Source/FrontEnd/LevelEditorController.java
@@ -1,9 +1,11 @@
 package FrontEnd;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
-
-import java.awt.*;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,17 +18,74 @@ import java.util.ResourceBundle;
  */
 public class LevelEditorController extends StateLoad {
 
+    @FXML
+    private Slider straightSlider;
+    @FXML
+    private TextField straightInBox;
+    @FXML
+    private Slider goalSlider;
+    @FXML
+    private TextField goalInBox;
+    @FXML
+    private Slider tshapeSlider;
+    @FXML
+    private TextField tshapeInBox;
+    @FXML
+    private Slider conerSlider;
+    @FXML
+    private TextField cornerInBox;
+    @FXML
+    private Slider fireSlider;
+    @FXML
+    private TextField fireInBox;
+    @FXML
+    private Slider iceSlider;
+    @FXML
+    private TextField iceInBox;
+    @FXML
+    private Slider doublemoveSlider;
+    @FXML
+    private TextField doublemoveInBox;
+    @FXML
+    private Slider backtrackSlider;
+    @FXML
+    private TextField backtrackInBox;
 
 
-    public LevelEditorController() {
 
-    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+
+        straightSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                straightInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+        conerSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                cornerInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+        tshapeSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                tshapeInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+        goalSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                goalInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+
+        fireSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                fireInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+        iceSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                iceInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+        backtrackSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                backtrackInBox.setText(String.valueOf(Math.round((Double) newValue))));
+
+        doublemoveSlider.valueProperty().addListener((observable, oldValue, newValue) ->
+                doublemoveInBox.setText(String.valueOf(Math.round((Double) newValue))));
     }
 
 
+
+    //Silk Bag Controls
 
 }
