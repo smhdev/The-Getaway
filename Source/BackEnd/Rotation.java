fb0e1d2f290efac4985b694bedcbbde7787af280
Chuzzy <thechuzzyguy@gmail.com>
2021-03-05 07:34:32 +0000

    Add Rotation.clockwise

--- Source/BackEnd/Rotation.java
+++ Source/BackEnd/Rotation.java
@@ -23,5 +23,20 @@ public enum Rotation {
                 throw new IllegalStateException("Unknown rotation " + this);
         }
     }
+
+    public Rotation clockwise() {
+        switch (this) {
+            case UP:
+                return RIGHT;
+            case RIGHT:
+                return DOWN;
+            case DOWN:
+                return LEFT;
+            case LEFT:
+                return UP;
+            default:
+                throw new IllegalStateException("Unknown rotation " + this);
+        }
+    }
 }
 
