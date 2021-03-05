6ae490f7f2b12a3f16a1e2801068d418afa4ef07
Chuzzy <thechuzzyguy@gmail.com>
2021-03-05 07:26:07 +0000

    Add Rotation.degrees

--- Source/BackEnd/Rotation.java
+++ Source/BackEnd/Rotation.java
@@ -7,6 +7,21 @@ package BackEnd;
  * @version 1.0
  */
 public enum Rotation {
-    UP, RIGHT, DOWN, LEFT
+    UP, RIGHT, DOWN, LEFT;
+
+    public double degrees() {
+        switch (this) {
+            case UP:
+                return 0;
+            case RIGHT:
+                return 90;
+            case DOWN:
+                return 180;
+            case LEFT:
+                return 270;
+            default:
+                throw new IllegalStateException("Unknown rotation " + this);
+        }
+    }
 }
 
